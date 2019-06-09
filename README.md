538a36a333ba732e9e28dbdc528967ed76f6b791
Carrie Forde <carrie@carrieforde.com>
2019-06-09 10:33:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# cf-alert
+
+A simple alert web component.
+
+## Usage
+
+In an HTML file:
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script type="module" src="components/alert/cf-alert.js"></script>
+
+    <!-- 
+        Use CSS custom properties to alter styles within the component.
+    -->
+    <style>
+        --alert-bg-default: hotpink;
+    </style>
+</head>
+<body>
+    <cf-alert>
+        This is the default alert.
+    </cf-alert>
+
+    <cf-alert type="info">
+        This is an info alert.
+    </cf-alert>
+
+    <cf-alert type="tip">
+        This is an tip / success alert.
+    </cf-alert>
+
+    <cf-alert type="warning">
+        This is an warning alert.
+    </cf-alert>
+
+    <cf-alert type="danger">
+        This is an danger alert.
+    </cf-alert>
+</body>
+</html>
+```
+
+With npm:
+
+```
+npm install cf-alert
+```
+
+```js
+import 'cf-alert';
+```
+
+## Configurable styles
+
+Each type of alert can be customized by changing the background color or the text color. Use the following custom properties for customization:
+
+```css
+/* Default */
+--alert-bg-default: #f0f4f8; /* background-color */
+--alert-color-default: #334e68; /* text color */
+
+/* Info */
+--alert-bg-info: #e3f8ff;
+--alert-color-info: #035388;
+
+/* Tip / Success */
+--alert-bg-tip: #effcf6;
+--alert-color-tip: #014d40;
+
+/* Warning */
+--alert-bg-warning: #fffbea;
+--alert-color-warning: #8d2b0b;
+
+/* Danger */
+--alert-bg-danger: #ffe3e3;
+--alert-color-danger: #610316;
+```
---
+++ cf-alert.js
@@ -0,0 +1,73 @@
+import { LitElement, html, css } from 'lit-element';
+import * as cfIcons from './cf-icons';
+
+class CfAlert extends LitElement {
+
+    static get styles() {
+        return css`
+            :host {
+                background-color: var(--alert-bg-default, #f0f4f8);
+                display: block;
+                color: var(--alert-color-default, #334e68);
+                padding: 12px 24px;
+            }
+
+            :host([type="info"]) {
+                background-color: var(--alert-bg-info, #e3f8ff);
+                color: var(--alert-color-info, #035388);
+            }
+
+            :host([type="tip"]),
+            :host([type="success"]) {
+                background-color: var(--alert-bg-tip, #effcf6);
+                color: var(--alert-color-tip, #014d40);
+            }
+
+            :host([type="warning"]) {
+                background-color: var(--alert-bg-warning, #fffbea);
+                color: var(--alert-color-warning, #8d2b0b);
+            }
+
+            :host([type="danger"]) {
+                background-color: var(--alert-bg-danger, #ffe3e3);
+                color: var(--alert-color-danger, #610316);
+            }
+
+            span {
+                display: inline-block;
+                width: 1rem;
+                margin-right: 8px;
+                vertical-align: middle;
+            }
+
+            :host([type="tip"]) span {
+                width: 0.75rem;
+            }
+
+            svg {
+                max-width: 100%;
+            }
+        `;
+    }
+
+    static get properties() {
+        return {
+            type: { type: String }
+        }
+    }
+
+    constructor() {
+        super();
+
+        this.type = '';
+    }
+
+    render() {
+        return html`
+            ${cfIcons[this.type] ? html`<span>${cfIcons[this.type]}</span>` : null}
+            <slot></slot>
+        `;
+    }
+}
+
+customElements.define('cf-alert', CfAlert);
